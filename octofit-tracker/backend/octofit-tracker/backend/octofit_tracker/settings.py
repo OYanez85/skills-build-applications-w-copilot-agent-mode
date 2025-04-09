1cea97d05eb0b04ae89c7057608f71e7d503721a
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2025-04-09 06:26:17 +0000

    Updated settings.py to use djongo for MongoDB configuration

--- octofit-tracker/backend/octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit-tracker/backend/octofit_tracker/settings.py
@@ -72,10 +72,15 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# Update the database configuration to use MongoDB with djongo
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "djongo",
+        "NAME": "octofit_db",
+        "ENFORCE_SCHEMA": False,
+        "CLIENT": {
+            "host": "mongodb://localhost:27017",
+        }
     }
 }
 
