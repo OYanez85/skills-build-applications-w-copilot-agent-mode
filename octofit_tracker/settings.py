6cceb8c8a9441cc17024659da05560944d46e45f
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2025-04-09 07:18:58 +0000

    Pushed all recent changes to the repository

--- octofit_tracker/settings.py
+++ octofit_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^*kv6407de3&3#i2i4$qbur5y8sn=t4!5tg)yjev9ql)welr86"
+SECRET_KEY = config("SECRET_KEY", default="django-insecure-default-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 CORS_ALLOW_ALL_ORIGINS = True
@@ -81,9 +82,9 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "djongo",
-        "NAME": "octofit_db",
-        "HOST": "localhost",
-        "PORT": 27017,
+        "NAME": config("DB_NAME", default="octofit_db"),
+        "HOST": config("DB_HOST", default="localhost"),
+        "PORT": config("DB_PORT", default=27017, cast=int),
     }
 }
 
